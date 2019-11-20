507a3865da94f859ab06e4de582183528d3802f0
tephron4 <tobiasephron@gmail.com>
2019-11-20 18:22:34 -0500

    made Star class

---
+++ Star.java
@@ -0,0 +1,13 @@
+public class Star{
+    
+    double radialVel;
+    double parallax;
+    String spectralType;
+
+    Star(double radVel, double parallaxes, String specType){
+	radialVel = radVel;
+	parallax = parallaxes;
+	spectralType = specType;
+    }
+
+}
